2e47fe3adeb564b35431ebabbd4206603ec626c6
Nathan Houle <nathan@nathanhoule.com>
2016-09-27 13:40:55 -0700

    Omit _metadata from #json

--- test/facade.test.js
+++ test/facade.test.js
@@ -220,6 +220,11 @@ describe('Facade', function() {
       var track = new Facade.Track({});
       assert.deepEqual(track.json().type, 'track');
     });
+
+    it('should omit _metadata', function() {
+      var track = new Facade.Track({ _metadata: {} });
+      assert.deepEqual(track.json()._metadata, undefined);
+    });
   });
 
   describe('.context()', function() {
