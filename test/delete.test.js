5b42de8027f54f09c98da2d1450f6f0fce32c6d7
Netto Farah <nettofarah@gmail.com>
2020-09-22 15:46:31 -0300

    Convert imports

--- test/delete.test.js
+++ test/delete.test.js
@@ -1,23 +1,23 @@
-'use strict';
+"use strict";
 
-var Delete = require('../lib').Delete;
-var assert = require('assert');
+import { Delete } from "../lib";
+import { strictEqual, deepEqual } from "assert";
 
-describe('Delete', function() {
+describe("Delete", function () {
   var obj = {
-    userId: '1'
+    userId: "1",
   };
-  var delete_ = new Delete(obj);  // delete is a reserved keyword in js
+  var delete_ = new Delete(obj); // delete is a reserved keyword in js
 
-  describe('.type()', function() {
-    it('should have the proper .type()', function() {
-      assert.strictEqual(delete_.type(), 'delete');
+  describe(".type()", function () {
+    it("should have the proper .type()", function () {
+      strictEqual(delete_.type(), "delete");
     });
   });
 
-  describe('.userId()', function() {
-    it('should proxy the userId', function() {
-      assert.deepEqual(delete_.userId(), obj.userId);
+  describe(".userId()", function () {
+    it("should proxy the userId", function () {
+      deepEqual(delete_.userId(), obj.userId);
     });
   });
 });
